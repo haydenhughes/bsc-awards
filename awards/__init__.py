5b9ec66399a0511c3f4ee46b808707a9280a441d
Hayden Hughes <haydenhughes@protonmail.com>
2018-06-23 07:22:01 +0000

    Revert "Merge branch 'rework_gitlab_pipeline' into 'master'"
    This reverts merge request !17

--- awards/__init__.py
+++ awards/__init__.py
@@ -1,54 +1,54 @@
+import unittest
 import sass
 import os
 from flask_sqlalchemy import SQLAlchemy
 from sassutils.wsgi import SassMiddleware
 from shutil import copyfile
 from flask import Flask
+from awards import config, main
 
-db = SQLAlchemy()
+
+app = Flask(__name__)
+app.config.from_object(config.Config)
+
+db = SQLAlchemy(app)
+
+# Register blueprints
+app.register_blueprint(main.bp)
 
 
-def create_app(config=None):
-    """Create a flask app.
+@app.cli.command()
+def test():
+    app.logger.info('Running tests...')
+    test_loader = unittest.TestLoader()
+    suite = test_loader.discover('test', pattern='test_*.py')
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
 
-    Args:
-        config: A object from config.py
-    """
-    app = Flask(__name__)
-    app.config.from_object(config)
 
-    db.init_app(app)
+def get_path(path):
+    current_dir = os.path.dirname(__file__)
+    return os.path.join(current_dir, path)
 
-    from awards import main
-    app.register_blueprint(main.bp)
 
-    def get_path(path):
-        current_dir = os.path.dirname(__file__)
-        return os.path.join(current_dir, path)
+app.logger.info('Compiling Bootstrap...')
+sass.compile(
+    dirname=(get_path('node_modules/bootstrap/scss/'), get_path('static/css')))
+if not os.path.exists(get_path('static/css')):
+    os.makedirs(get_path('static/css'))
+if not os.path.exists(get_path('static/js')):
+    os.makedirs(get_path('static/js'))
 
-    app.logger.info('Compiling Bootstrap...')
+copyfile(get_path('node_modules/bootstrap/dist/js/bootstrap.min.js'),
+         get_path('static/js/bootstrap.min.js'))
+copyfile(get_path('node_modules/jquery/dist/jquery.min.js'),
+         get_path('static/js/query.min.js'))
+copyfile(get_path('node_modules/popper.js/dist/umd/popper.min.js'),
+         get_path('static/js/popper.min.js'))
 
-    sass.compile(
-        dirname=(get_path('node_modules/bootstrap/scss/'), get_path('static/css')))
+app.logger.info('Done!')
 
-    if not os.path.exists(get_path('static/css')):
-        os.makedirs(get_path('static/css'))
-    if not os.path.exists(get_path('static/js')):
-        os.makedirs(get_path('static/js'))
-
-    copyfile(get_path('node_modules/bootstrap/dist/js/bootstrap.min.js'),
-             get_path('static/js/bootstrap.min.js'))
-    copyfile(get_path('node_modules/jquery/dist/jquery.min.js'),
-             get_path('static/js/query.min.js'))
-    copyfile(get_path('node_modules/popper.js/dist/umd/popper.min.js'),
-             get_path('static/js/popper.min.js'))
-
-    app.logger.info('Done!')
-
-    app.logger.info('Compiling sass for the first time...')
-
-    app.wsgi_app = SassMiddleware(app.wsgi_app, {
-        'awards': ('static/sass', 'static/css', '/static/css')
-    })
-
-    return app
+app.logger.info('Compiling sass for the first time...')
+app.wsgi_app = SassMiddleware(app.wsgi_app, {
+    'awards': ('static/sass', 'static/css', '/static/css')
+})
