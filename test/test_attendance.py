b13081c820814d950eafc64352f1c0c72c9eaf87
Hayden Hughes <haydenhughes@protonmail.com>
2018-06-23 15:43:53 +1000

    Move the setup into test_attendance.py

--- test/test_attendance.py
+++ test/test_attendance.py
@@ -1,7 +1,25 @@
-from test import db_testcase
+import unittest
+from awards import models, db, create_app, attendance
+from config import Testing
 
 
-class TestAttentanceTracker(db_testcase.DBTestCase):
+class TestAttentanceTracker(unittest.TestCase):
+    def setUpClass(self):
+        self.app = create_app(Testing)
+        self.attendance_tracker = attendance.AttendanceTracker()
+
+        student_list = [('HUG0005', 'Sam', 'Wilson', True),
+                        ('WIL0123', 'Jake', 'Bruckner', False),
+                        ('ROB2134', 'Ben', 'Hughes', True)]
+
+        for student_id, first_name, last_name, attending in student_list:
+            student = models.Student(student_id=student_id,
+                                     first_name=first_name,
+                                     last_name=last_name,
+                                     attending=attending)
+            db.session.add(student)
+        db.session.commit()
+
     def test_get(self):
         self.assertTrue(self.attendance_tracker['HUG0005'])
         self.assertFalse(self.attendance_tracker['WIL0123'])
