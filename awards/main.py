59257f30b58d70db064509b35346f6f9895c6098
Hayden Hughes <haydenhughes@protonmail.com>
2018-07-04 16:09:06 +0200

    Create a test for the main view

--- awards/main.py
+++ awards/main.py
@@ -3,6 +3,6 @@ from flask import Blueprint, render_template
 bp = Blueprint('main', __name__)
 
 
-@bp.route('/')
+@bp.route('/main')
 def index():
     return render_template('header.html')
--- test/test_views.py
+++ test/test_views.py
@@ -10,3 +10,7 @@ class TestViews(unittest.TestCase):
     def test_attendance(self):
         response = self.app.get('/attendance', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
+
+    def test_main(self):
+        response = self.app.get('/attendance', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
