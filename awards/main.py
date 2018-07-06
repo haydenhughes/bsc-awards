5e2a064f470c8d27db2a4f0cae19a647fdbe5e21
Hayden Hughes <haydenhughes@protonmail.com>
2018-07-06 14:42:50 +0200

    Get the year levels from a the app config and use a for loop to display

--- awards/main.py
+++ awards/main.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app
 
 bp = Blueprint('main', __name__)
 
@@ -6,3 +7,6 @@ bp = Blueprint('main', __name__)
 @bp.route('/main')
 def index():
     return render_template('header.html')
+              'Actually Comes To School Award']
+    return render_template('main/index.html',
+                           year_levels=current_app.config['YEAR_LEVELS'],
--- config.py
+++ config.py
@@ -4,4 +4,6 @@ import os
 class Config:
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
+    YEAR_LEVELS = ['Year 7', 'Year 8', 'Year 9',
+                   'Year 10', 'Year 11', 'Year 12']
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URI']
