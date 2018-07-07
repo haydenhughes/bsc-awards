98219fc2dc9d2a476de577c3cdcb6ccedb7ed40e
Hayden Hughes <haydenhughes@protonmail.com>
2018-07-07 14:53:32 +0200

    Remove __setitem__ to instead use a context manager

--- awards/attendancetools.py
+++ awards/attendancetools.py
@@ -4,16 +4,14 @@ from awards import db, models
 class AttendanceTracker:
     """A helper object for managing student attendance."""
 
-    def _get_student(self, student_id):
-        return models.Student.query.filter_by(student_id=student_id).first()
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        db.session.commit()
 
     def __iter__(self):
         return iter(models.Student.query.filter_by(attending=True).all())
 
     def __getitem__(self, index):
-        return self._get_student(index).attending
-
-    def __setitem__(self, index, value):
-        student = self._get_student(index)
-        student.attending = value
-        db.session.commit()
+        return models.Student.query.filter_by(student_id=index).first()
