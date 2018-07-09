73c800238963f24bf8a3912e698b274cacb60ea3
Hayden Hughes <haydenhughes@protonmail.com>
2018-07-09 17:02:39 +0200

    Reformat the YEAR_LEVELS array

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ import os
 class Config:
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
-    YEAR_LEVELS = ['Year 7', 'Year 8', 'Year 9',
-                   'Year 10', 'Year 11', 'Year 12']
+    YEAR_LEVELS = ['year_7', 'year_8', 'year_9',
+                   'year_10', 'year_11', 'year_12']
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URI']
