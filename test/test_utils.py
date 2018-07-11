80df9433fd7d4e969b8e84bc8939d71cf87345b7
Hayden Hughes <haydenhughes@protonmail.com>
2018-07-11 11:16:24 +0200

    Move all of the tests using the DB into one class

--- test/test_utils.py
+++ test/test_utils.py
@@ -2,64 +2,9 @@ import unittest
 from awards import models, db, create_app, utils
 
 
-class TestGroupSize(unittest.TestCase):
-    def test_group_size(self):
-        self.assertEqual(utils.group_size(60).size, 9)
-        self.assertEqual(utils.group_size(60).count, 6)
-        self.assertEqual(utils.group_size(60).last_size, 6)
-
-        self.assertEqual(utils.group_size(49).size, 7)
-        self.assertEqual(utils.group_size(49).count, 7)
-        self.assertEqual(utils.group_size(49).last_size, 0)
-
-        self.assertEqual(utils.group_size(75).size, 7)
-        self.assertEqual(utils.group_size(75).count, 10)
-        self.assertEqual(utils.group_size(75).last_size, 5)
-
-        self.assertEqual(utils.group_size(51).size, 9)
-        self.assertEqual(utils.group_size(51).count, 5)
-        self.assertEqual(utils.group_size(51).last_size, 6)
-
-
-class TestStudentManager(unittest.TestCase):
-    def setUp(self):
-        app = create_app()
-        app.app_context().push()
-
-        db.create_all()
-
-        self.sm = utils.StudentManager()
-
-        student_list = [('HUG0005', 'Sam', 'Wilson', True),
-                        ('WIL0123', 'Jake', 'Bruckner', False),
-                        ('ROB2134', 'Ben', 'Hughes', True)]
-
-        for student_id, first_name, last_name, attending in student_list:
-            student = models.Student(student_id=student_id,
-                                     first_name=first_name,
-                                     last_name=last_name,
-                                     attending=attending)
-            db.session.add(student)
-
-        db.session.commit()
-
-    def test_get(self):
-        self.assertTrue(self.sm.get('HUG0005').attending)
-        self.assertFalse(self.sm.get('WIL0123').attending)
-
-    def test_get_by_index(self):
-        self.assertEqual(self.sm[0].student_id, 'HUG0005')
-        self.assertEqual(self.sm[2].student_id, 'ROB2134')
-
-        with self.assertRaises(IndexError):
-            self.assertEqual(self.sm[3])
-
-    def test_len(self):
-        self.assertEqual(len(self.sm), 3)
-
-
-class TestGetAwards(unittest.TestCase):
-    def setUp(self):
+class TestDB(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
         app = create_app()
         app.app_context().push()
 
@@ -94,5 +39,45 @@ class TestGetAwards(unittest.TestCase):
 
     db.session.commit()
 
-    def test(self):
+    def setUp(self):
+        self.sm = utils.StudentManager()
+
+    def test_StudentManager_get(self):
+        self.assertTrue(self.sm.get('HUG0005').attending)
+        self.assertFalse(self.sm.get('WIL0123').attending)
+
+    def test_StudentManager_get_by_index(self):
+        self.assertEqual(self.sm[0].student_id, 'HUG0005')
+        self.assertEqual(self.sm[2].student_id, 'ROB2134')
+
+        with self.assertRaises(IndexError):
+            self.assertEqual(self.sm[3])
+
+    def test_StudentManager_len(self):
+        self.assertEqual(len(self.sm), 3)
+
+    def test_get_awards(self):
         self.assertEqual(utils.get_awards('ROB2134'), 'Very Special Award')
+
+    @classmethod
+    def tearDownClass():
+        pass  # TODO: Delete all the setup db tables
+
+
+class TestGroupSize(unittest.TestCase):
+    def test_group_size(self):
+        self.assertEqual(utils.group_size(60).size, 9)
+        self.assertEqual(utils.group_size(60).count, 6)
+        self.assertEqual(utils.group_size(60).last_size, 6)
+
+        self.assertEqual(utils.group_size(49).size, 7)
+        self.assertEqual(utils.group_size(49).count, 7)
+        self.assertEqual(utils.group_size(49).last_size, 0)
+
+        self.assertEqual(utils.group_size(75).size, 7)
+        self.assertEqual(utils.group_size(75).count, 10)
+        self.assertEqual(utils.group_size(75).last_size, 5)
+
+        self.assertEqual(utils.group_size(51).size, 9)
+        self.assertEqual(utils.group_size(51).count, 5)
+        self.assertEqual(utils.group_size(51).last_size, 6)
