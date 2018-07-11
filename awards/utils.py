7a28a009e702eaf05a24795c72263b74bdfd2843
Hayden Hughes <haydenhughes@protonmail.com>
2018-07-11 19:52:14 +0200

    Utils has goten ahead of its branch

--- awards/utils.py
+++ awards/utils.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 import math
-from awards import db, models
+from awards import db
 
 
 class Utils:
@@ -20,30 +20,3 @@ class Utils:
                 groups.count = math.floor(student_count / group_size)
                 groups.last_size = student_count % group_size
         return groups
-
-
-class StudentManager:
-    """Manages student information.
-
-    Args:
-        year_level: A string to specify what year level to work with.
-                    None for all (default).
-    """
-
-    def __init__(self, year_level=None):
-        self.year_level = year_level
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, *args):
-        db.session.commit()
-
-    def __len__(self):
-        len(models.Student.query.filter_by(attending=True, year_level=self.year_level).all())
-
-    def __iter__(self):
-        return iter(models.Student.query.filter_by(attending=True, year_level=self.year_level).all())
-
-    def __getitem__(self, index):
-        return models.Student.query.filter_by(student_id=index, year_level=self.year_level).first()
