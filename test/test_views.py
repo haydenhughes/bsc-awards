6eadee1d3e16dc2a7c843fcf87ed4cb7e5526522
Hayden Hughes <haydenhughes@protonmail.com>
2018-07-22 09:51:31 +0200

    Add attendance search test

--- test/test_views.py
+++ test/test_views.py
@@ -1,16 +1,30 @@
 import unittest
-from awards import create_app
+import random
+from awards import create_app, utils
 from mockdb import MockDB
 
 
 class TestAttendance(unittest.TestCase):
     def setUp(self):
-        app = create_app()
-        self.app = app.test_client()
+        self.app = create_app()
+        self.client = self.app.test_client()
+        self.md = MockDB()
+        self.md.setUp()
+        self.sm = utils.StudentManager()
 
-    def test_attendance(self):
-        response = self.app.get('/attendance', follow_redirects=True)
+    def test_url(self):
+        response = self.client.get('/attendance', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
+
+    def test_student_search(self):
+        student_id = random.choice(self.md.student_ids)
+        student = self.sm.get(student_id)
+        rv = self.client.get('/attendance?studentID={}'.format(student_id), follow_redirects=True)
+
+        self.assertTrue(str.encode('{} {}'.format(student.first_name, student.last_name)) in rv.data)
+
+    def tearDown(self):
+        self.md.tearDown()
 
 
 class TestMain(unittest.TestCase):
@@ -22,11 +36,11 @@ class TestMain(unittest.TestCase):
 
     def test_main(self):
         for year_level in self.app.config['YEAR_LEVELS']:
-            response = self.test_client.get('/main/{}/0/0'.format(str(year_level)))
+            response = self.test_client.get('/main/{}/0/0'.format(str(year_level)), follow_redirects=True)
             self.assertEqual(response.status_code, 200)
 
         invalid_year = self.app.config['YEAR_LEVELS'][0] - 1
-        response = self.test_client.get('/main/{}/0/0'.format(str(invalid_year)))
+        response = self.test_client.get('/main/{}/0/0'.format(str(invalid_year)), follow_redirects=True)
         self.assertEqual(response.status_code, 404)
 
     def tearDown(self):
