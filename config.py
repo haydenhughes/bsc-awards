73691ed35034c70a3310c0b06a3b9113d81996f6
Hayden Hughes <haydenhughes@protonmail.com>
2018-07-29 21:02:34 +0200

    Add password hashing

--- config.py
+++ config.py
@@ -2,10 +2,14 @@ import os
 
 
 class Config:
+    SECURITY_PASSWORD_HASH = 'bcrypt'
+    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SECURITY_PASSWORD_SALT = 'change-me-as-well'
     NAVBAR_BRAND = 'BSC Awards'
-    ADMIN_EMAIL = os.environ['ADMIN_EMAIL']
-    ADMIN_PASSWORD = os.environ['ADMIN_PASSWORD']
+
     YEAR_LEVELS = [7, 8, 9, 10, 11, 12]
     CSRF_ENABLED = True
-    SECRET_KEY = 'this-really-needs-to-be-changed'
+
+    ADMIN_EMAIL = os.environ['ADMIN_EMAIL']
+    ADMIN_PASSWORD = os.environ['ADMIN_PASSWORD']
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URI']
