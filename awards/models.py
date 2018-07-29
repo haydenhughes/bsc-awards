8570d77c73cb7787d5316e54d3bd26ec56fc6f31
Hayden Hughes <haydenhughes@protonmail.com>
2018-07-29 12:33:35 +0200

    Add user and role models

--- awards/models.py
+++ awards/models.py
@@ -1,4 +1,5 @@
 from awards import db
+from flask_security import UserMixin, RoleMixin
 
 
 class AwardRecipients(db.Model):
@@ -35,3 +36,19 @@ class Awards(db.Model):
     special_award = db.Column(db.Boolean)
     number_of_awards = db.Column(db.Integer)
     prize = db.Column(db.String(120))
+
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
