b13ade9c8270cbf8dea4e312250c32770b9d126f
Hayden Hughes <haydenhughes@protonmail.com>
2018-07-29 12:44:59 +0200

    Add ADMIN_EMAIL and ADMIN_PASSWORD environment variables

--- config.py
+++ config.py
@@ -2,8 +2,10 @@ import os
 
 
 class Config:
+    NAVBAR_BRAND = 'BSC Awards'
+    ADMIN_EMAIL = os.environ['ADMIN_EMAIL']
+    ADMIN_PASSWORD = os.environ['ADMIN_PASSWORD']
+    YEAR_LEVELS = [7, 8, 9, 10, 11, 12]
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
-    YEAR_LEVELS = [7, 8, 9, 10, 11, 12]
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URI']
-    NAVBAR_BRAND = 'BSC Awards'
