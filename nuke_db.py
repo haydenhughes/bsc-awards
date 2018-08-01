30b701cd443a0685558d65c0c3eb8b117912731f
Hayden Hughes <haydenhughes@protonmail.com>
2018-08-01 11:24:20 +0200

    Create nuke_db

---
+++ nuke_db.py
@@ -0,0 +1,16 @@
+from awards import db, models, create_app
+
+if __name__ == '__main__':
+    if input('Are you sure you want to nuke all the tables in the database from space? (y/N) ') == 'y':
+        app = create_app()
+        app.app_context().push()
+        models.Student.query.delete()
+        models.Awards.query.delete()
+        models.AwardRecipients.query.delete()
+        models.User.query.delete()
+        models.Role.query.delete()
+        db.session.commit()
+        print('Done!')
+    else:
+        print('Canceled!')
+
