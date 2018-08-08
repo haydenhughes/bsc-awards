bdc6a40c1275db1b71f889d1050ee16931993aa5
Hayden Hughes <haydenhughes@protonmail.com>
2018-08-08 12:14:07 +1000

    Update enviroment variables

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ class Config:
     YEAR_LEVELS = [7, 8, 9, 10, 11, 12]
     CSRF_ENABLED = True
 
-    USERNAME = os.environ['USERNAME']
-    PASSWORD = os.environ['PASSWORD']
+    USERNAME = os.environ['AWARDS_USERNAME']
+    PASSWORD = os.environ['AWARDS_PASSWORD']
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URI']
