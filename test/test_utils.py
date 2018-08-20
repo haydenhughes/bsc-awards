a3698ec5a3f86904f61d10a8e5bc0bc443dca717
Hayden Hughes <hayden@bitmessage.ch>
2018-08-20 11:19:22 +1000

    Fix syntax error

--- test/test_utils.py
+++ test/test_utils.py
@@ -45,34 +45,8 @@ class TestGetAwards(unittest.TestCase):
         for student_id in self.mock_db.student_ids:
             awards = [award.award_id for award in models.AwardRecipients.query.filter_by(
                 student_id=student_id).all()]
-            self.assertCountEqual(list(utils.get_awards(student_id), awards)
+            self.assertCountEqual(list(utils.get_awards(student_id), awards))
 
     def tearDown(self):
         self.mock_db.tearDown()
 
-
-# class TestGroupManager(unittest.TestCase):
-#     def setUp(self):
-#         self.md = MockDB(student_count=20, year_level=[7])
-#         self.md.setUp()
-
-#         self.gm = utils.GroupManager()
-#         self.gm.sm.allow_no_awards = True
-
-#     def test_attributes(self):
-#         self.assertEqual(self.gm.size, 7)
-#         self.assertEqual(self.gm.count, 2)
-#         self.assertEqual(self.gm.last_size, 6)
-
-#     def test_get(self):
-#         self.assertIsNotNone(self.gm[0][0])
-#         self.assertIsNotNone(self.gm[1][6])
-
-#         with self.assertRaises(IndexError):
-#             self.gm[0][7]
-
-#         with self.assertRaises(IndexError):
-#             self.gm[3][0]
-
-#     def tearDown(self):
-#         self.md.tearDown()
