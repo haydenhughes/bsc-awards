9d1e06cb07a5027863386c056883b3ec50f6f4b6
Hayden Hughes <hayden@vulpes.chickenkiller.com>
2019-09-30 10:06:50 +1000

    Delete unused deploy.sh

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t haydenhughes/bsc-awards:$TRAVIS_TAG -t haydenhughes/bsc-awards:latest  .
-docker push haydenhughes/bsc-awards:$TRAVIS_TAG
-docker push haydenhughes/bsc-awards:latest
